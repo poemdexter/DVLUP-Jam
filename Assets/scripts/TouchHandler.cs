8a1a74a94eea5a1f00c662f5a1c7fbf808e35412
Daniel Fairley <dfairley@credera.com>
2014-05-17 15:43:45 -0500

    added touch handler work in progress.

---
+++ Assets/scripts/TouchHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchHandler : MonoBehaviour
+{
+    public float deadZone;
+    private bool isTouching = false;
+    private Vector3 startPosition;
+
+    void Update()
+    {
+        // Look for all fingers
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            
+            // drag
+            if (touch.phase == TouchPhase.Began) {
+                startPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                isTouching = true;
+            } else if (touch.phase == TouchPhase.Moved) {
+                Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+                if (Mathf.Abs(position.y - startPosition.y) >= 1) {
+                    Debug.Log("moved 1");
+                    startPosition = position;
+                }
+            } else if (touch.phase == TouchPhase.Ended) {
+                isTouching = false;
+            }
+        }
+    }
+}
