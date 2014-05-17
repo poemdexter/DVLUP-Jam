fd18910f0f3b919e88af86ee7381839b1ea462a6
Daniel Fairley <dfairley@credera.com>
2014-05-17 12:23:35 -0500

    characters moving.

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public bool isPlayer;
+    public float moveDelay = 3f;
+	
+    private float currentTime = 0;
+
+    void Update()
+    {
+        if ((currentTime += Time.deltaTime) >= moveDelay) {
+            Move();
+            currentTime = 0;
+        }
+    }
+
+    private void Move()
+    {
+        float direction = (isPlayer) ? 1 : -1;
+        transform.Translate(direction * Vector2.right);
+    }
+}
