f237f7d6e19c1777a9245eb405fd12188f29a358
Yngar <yngarthewise@gmail.com>
2014-05-17 14:05:43 -0500

    Added variables to mob.cs

--- Assets/scripts/Mob.cs
+++ Assets/scripts/Mob.cs
@@ -4,4 +4,8 @@ using System.Collections;
 public class Mob : MonoBehaviour
 {
     public Vector2 gridPosition;
+	public int xRange;
+	public int yRange;
+	public int health;
+	public int damage;
 }
